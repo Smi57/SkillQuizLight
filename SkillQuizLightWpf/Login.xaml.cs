861a1aec473a81ebfb50d0095a2a5b23f6e526d5
Sébastien MINSTER <minstse@aubay.be>
2023-04-26 17:08:10 +0200

    001b

--- SkillQuizLightWpf/Login.xaml.cs
+++ SkillQuizLightWpf/Login.xaml.cs
@@ -10,21 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SkillQuizLightWpf
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-            Login login = new Login();
-            login.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();    
         }
     }
 }
